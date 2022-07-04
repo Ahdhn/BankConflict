eb030ac99980670383db92e83f1fad0bceb886fa
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-07-04 16:42:51 -0400

    Fix

--- BankConflict/include/Block3D.cuh
+++ BankConflict/include/Block3D.cuh
@@ -10,37 +10,41 @@ template <int xSize, int ySize, int zSize, int radius, typename T>
 __global__ void kernel_3d(T* d_output, int num_repeat)
 {
     constexpr int shmem_size =
-        (xSize + radius) * (ySize + radius) * (zSize + radius);
+        (xSize + 2 * radius) * (ySize + 2 * radius) * (zSize + 2 * radius);
+
+    assert(blockDim.x == xSize && blockDim.y == ySize && blockDim.z == zSize);
+    assert(gridDim.y == 1 && gridDim.z == 1);
 
     // clang-format off
     const int read_id = threadIdx.x +
                         threadIdx.y * xSize +
                         threadIdx.z * xSize * ySize;
    
-    const int block_id = blockIdx.x * blockDim.x * blockDim.y * blockDim.z;
+    
+    const int block_stride = blockIdx.x * xSize * ySize * zSize;
 
-     int write_id = (threadIdx.x + radius) +
-                    (threadIdx.y + radius) * xSize +
-                    (threadIdx.z + radius) * xSize * ySize;
+    const int write_id = (threadIdx.x + radius) +
+                         (threadIdx.y + radius) * (xSize + 2 * radius) +
+                         (threadIdx.z + radius) * (xSize + 2 * radius) * (ySize + 2 * radius);
     // clang-format on
 
     __shared__ T s_mem[shmem_size];
 
-    for (int i = read_id; i < shmem_size;
-         i += blockDim.x * blockDim.y * blockDim.z) {
+    for (int i = read_id; i < shmem_size; i += xSize * ySize * zSize) {
         s_mem[i] = 0;
     }
     __syncthreads();
 
+    int id = write_id;
     for (int i = 0; i < num_repeat; i++) {
-        s_mem[write_id] += write_id + i;
-        write_id += 32;
-        write_id %= shmem_size;
+        s_mem[id] += read_id + i;
+        id += 32;
+        id %= shmem_size;
     }
 
     __syncthreads();
 
-    d_output[block_id + read_id] = s_mem[read_id];
+    d_output[block_stride + read_id] = s_mem[write_id];
 }
 
 template <int radius>
@@ -71,6 +75,15 @@ void run_test_3d(size_t size_1d, int num_repeat, int num_run)
     }
     CUDA_ERROR(cudaProfilerStop());
 
+
+    thrust::host_vector<T> h_output(d_input.size());
+    h_output = d_input;
+
+    for (uint32_t i = 0; i < h_output.size(); ++i) {
+        EXPECT_EQ(h_output[i], i % (xSize * ySize * zSize));
+    }
+
+
     std::cout << "\n**** size= " << size_1d * size_1d * size_1d
               << ", num_run = " << num_run << ", radius = " << radius
               << ", time(ms)= " << sum_time / float(num_run);
@@ -79,10 +92,10 @@ void run_test_3d(size_t size_1d, int num_repeat, int num_run)
 TEST(Test, run_3d)
 {
     const size_t size       = 1024;
-    const int    num_run    = 100;
+    const int    num_run    = 10;
     const int    num_repeat = 1;
     run_test_3d<0>(size, num_repeat, num_run);
-    // run_test_3d<1>(size, num_repeat, num_run);
+    run_test_3d<1>(size, num_repeat, num_run);
     // run_test_3d<2>(size, num_repeat, num_run);
     printf("\n");
 }
